0b5b351f9785dc38f305579af2c3172fa7ccbae4
JaneKim <90606616+jane-kim-279@users.noreply.github.com>
2023-08-02 21:47:24 +0900

    Create 77485.py

---
+++ 프로그래머스_문풀전략/3단원/77485.py
@@ -0,0 +1,36 @@
+'''
+행렬 테두리 회전하기
+https://school.programmers.co.kr/learn/courses/30/lessons/77485
+'''
+
+def rotate(x1, y1, x2, y2, matrix):
+    first = matrix[x1][y1]
+    min_value = first
+    
+    #왼쪽
+    for k in range(x1, x2):
+        matrix[k][y1] = matrix[k + 1][y1]
+        min_value = min(min_value, matrix[k + 1][y1])   
+    #아래
+    for k in range(y1, y2):
+        matrix[x2][k] = matrix[x2][k + 1]
+        min_value = min(min_value, matrix[x2][k + 1])
+    #오른쪽
+    for k in range(x2, x1, -1):
+        matrix[k][y2] = matrix[k - 1][y2]
+        min_value = min(min_value, matrix[k - 1][y2])
+    #위
+    for k in range(y2, y1 + 1, -1):
+        matrix[x1][k] = matrix[x1][k - 1]
+        min_value = min(min_value, matrix[x1][k - 1])
+        
+    matrix[x1][y1 + 1] = first
+    return min_value
+        
+def solution(rows, columns, queries):
+    matrix = [[(i) * columns + (j + 1) for j in range(columns)] for i in range(rows)]
+    answer = []
+    for x1, y1, x2, y2 in queries:
+        answer.append(rotate(x1 - 1, y1 - 1, x2 - 1, y2 - 1, matrix))
+    
+    return answer
