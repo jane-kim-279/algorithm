1245c40eeab2f5a195c085afa82c7c120de1e6cf
JaneKim <90606616+jane-kim-279@users.noreply.github.com>
2023-08-09 14:17:21 +0900

    Create 60057.py

---
+++ 프로그래머스_문풀전략/3단원/60057.py
@@ -0,0 +1,22 @@
+'''
+프로그래머스 문자열 압축 (2020 카카오 블라인드 문제)
+https://school.programmers.co.kr/learn/courses/30/lessons/60057
+'''
+
+def solution(s):
+    answer = len(s)
+    for x in range(1, len(s) // 2 + 1):
+        rep_len = 0
+        rep = ''
+        cnt = 1
+        for i in range(0, len(s) + 1, x):
+            temp = s[i:i + x]
+            if rep == temp: cnt += 1
+            elif rep != temp:
+                rep_len += len(temp)
+                if cnt > 1: rep_len += len(str(cnt))
+                cnt = 1
+                rep = temp
+        answer = min(answer, rep_len)
+    
+    return answer
