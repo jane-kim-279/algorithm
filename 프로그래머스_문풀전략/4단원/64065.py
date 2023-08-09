c7a8197621d8dd8164683b58564275c4e73358bd
JaneKim <90606616+jane-kim-279@users.noreply.github.com>
2023-08-09 14:22:55 +0900

    Create 64065.py

---
+++ 프로그래머스_문풀전략/4단원/64065.py
@@ -0,0 +1,16 @@
+'''
+프로그래머스 2019 카카오 개발자 겨울 인턴십 문제
+https://school.programmers.co.kr/learn/courses/30/lessons/64065
+'''
+
+def solution(s):
+    answer = {} #배열에서 딕셔너리로 교체
+    s = sorted(s[2:-2].split("},{"), key=len)
+    for tuples in s:
+        elements = tuples.split(',')
+        for element in elements:
+            number = int(element)
+            if number not in answer:
+                answer[number] = 1
+
+    return list(answer)
