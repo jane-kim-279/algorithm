6d0cb88510e4702e9a8000de6797a176597a9ca2
JaneKim <90606616+jane-kim-279@users.noreply.github.com>
2023-12-09 02:45:55 +0900

    Update DFS와BFS.py
    풀이 추가~

--- baekjoon/DFS와BFS.py
+++ baekjoon/DFS와BFS.py
@@ -1,7 +1,10 @@
 '''
 백준 DFS와 BFS
+https://www.acmicpc.net/problem/1260
+'''
 
 '''
+#1. True/False로 구현
 
 from collections import deque
   
@@ -36,6 +39,47 @@ def bfs(v):
         print(x, end=' ')
         for i in range(1, N+1):
             if not visited2[i] and graph[x][i]:
+                q.append(i)
+                visited2[i] = True
+
+dfs(V)
+print()
+bfs(V)
+'''
+
+#정점 저장만으로 풀이
+from collections import deque
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+
+visited1 = [False] * (N+1)
+visited2 = [False] * (N+1)
+
+for i in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
+
+def dfs(v):
+    visited1[v] = True
+    print(v, end=" ")
+    for i in graph[v]:
+        if not visited1[i]:
+            dfs(i)
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visited2[v] = True
+    while q:
+        x = q.popleft()
+        print(x, end=" ")
+        for i in graph[x]:
+            if not visited2[i]:
                 q.append(i)
                 visited2[i] = True
 
